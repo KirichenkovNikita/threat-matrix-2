1289cc6ecc941faa488a28a643c0be219b6147de
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-08-22 19:29:36 +0400

    update

--- examples/csharp/CollectionExtensions.cs
+++ examples/csharp/CollectionExtensions.cs
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repositories:
+*     - repo: https://github.com/aspnet/AspNetWebStack release version: v3.3.0  asset relative path: src/Common/CollectionExtensions.cs
+*    
+*    Copyrights:
+*    
+*    
+*    Licenses:
+*     - Apache License 2.0
+*       SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
