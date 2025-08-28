17bf4d09f8ab933da1a1a557456ca9ef22e4508e
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-08-28 18:09:49 +0400

    update

--- examples/c/mixed1.c
+++ examples/c/mixed1.c
@@ -3,15 +3,20 @@
 *    
 *    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
 *    
-*    Repository: https://github.com/eclipse/mosquitto
-*    Source File: src/mosquitto.c
+*    Repositories:
+*     - repo: https://github.com/eclipse/mosquitto/blob/v1.5.3/src/mosquitto.c release version: v1.5.3  asset relative path: src/mosquitto.c
+*     - repo: https://github.com/eclipse/mosquitto release version:   asset relative path: src/mosquitto.c
+*     - repo: https://github.com/eclipse/mosquitto/blob/v2.0.0/src/control.c release version: v2.0.0  asset relative path: src/control.c
+*     - repo: https://github.com/zephyrproject-rtos/zephyr/blob/v1.0.0/arch/arm/core/sys_fatal_error_handler.c release version: v1.0.0  asset relative path: arch/arm/core/sys_fatal_error_handler.c
 *    
 *    Copyrights:
-*      copyright (c) 2009-2018 roger light <roger@atchoo.org>
+*     - copyright (c) 2009-2018 roger light <roger@atchoo.org>
 *    
 *    Licenses:
-*      Eclipse Public License 1.0
-*      SPDXId: EPL-1.0
+*     - Eclipse Public License 2.0
+*       SPDXId: EPL-2.0
+*     - Eclipse Public License 1.0
+*       SPDXId: EPL-1.0
 *    
 *    Auto-attribution by Threatrix, Inc.
 *    
