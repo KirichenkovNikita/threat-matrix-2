150f607ffd38845c2dde2dfdb951a3f44743c5fa
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-08-28 18:12:01 +0400

    update

--- examples/c/my_call_chain.c
+++ examples/c/my_call_chain.c
@@ -1,3 +1,21 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repositories:
+*     - repo: https://github.com/torvalds/linux/blob/v5.8-rc1/kernel/events/callchain.c release version: v5.8-rc1  asset relative path: kernel/events/callchain.c
+*    
+*    Copyrights:
+*    
+*    Licenses:
+*     - GNU General Public License v2.0 only
+*       SPDXId: GPL-2.0-only
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 truct callchain_cpus_entries {
 	struct rcu_head			rcu_head;
 	struct perf_callchain_entry	*cpu_entries[];
