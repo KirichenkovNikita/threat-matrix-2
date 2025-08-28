d789d1850ac8f611a76b5bb4de12ba57a50e25c3
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-08-28 16:16:01 +0400

    update

--- examples/java/mixed1.java
+++ examples/java/mixed1.java
@@ -3,16 +3,18 @@
 *    
 *    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
 *    
-*    Repository: https://github.com/spring-projects/spring-security
-*    Source File: crypto/src/main/java/org/springframework/security/crypto/codec/Base64.java
+*    Repositories:
+*     - repo: https://github.com/spring-projects/spring-boot-data-geode/blob/1.3.0.RC1/spring-geode/src/main/java/org/springframework/geode/core/util/ObjectUtils.java release version: 1.3.0.RELEASE  asset relative path: spring-geode/src/main/java/org/springframework/geode/core/util/ObjectUtils.java
+*     - repo: https://github.com/spring-projects/spring-security release version:   asset relative path: crypto/src/main/java/org/springframework/security/crypto/codec/Base64.java
+*     - repo: https://github.com/spring-projects/spring-boot/blob/v2.4.10/spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/Connection.java release version: v2.6.15  asset relative path: spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/Connection.java
+*     - repo: https://github.com/spring-projects/spring-authorization-server/blob/0.1.1/oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/OAuth2TokenIntrospection.java release version: 0.3.1  asset relative path: oauth2-authorization-server/src/main/java/org/springframework/security/oauth2/core/OAuth2TokenIntrospection.java
 *    
 *    Copyrights:
-*      copyright 2002-2016 the original author or authors
+*     - copyright 2002-2016 the original author or authors
 *    
 *    Licenses:
-*      Apache License 2.0
-*      SPDXId: Apache-2.0
-*    
+*     - Apache License 2.0
+*       SPDXId: Apache-2.0
 *    
 *    Auto-attribution by Threatrix, Inc.
 *    
