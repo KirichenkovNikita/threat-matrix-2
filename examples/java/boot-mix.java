8d21bd8cb42929d3a81d4348194a6dd6c99635bf
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-08-28 16:25:28 +0400

    update

--- examples/java/boot-mix.java
+++ examples/java/boot-mix.java
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Repositories:
+*     - repo: https://github.com/spring-projects/spring-boot/blob/v2.0.0.M1/spring-boot-cli/src/main/java/org/springframework/boot/cli/SpringCli.java release version: v2.0.0.M1  asset relative path: spring-boot-cli/src/main/java/org/springframework/boot/cli/SpringCli.java
+*     - repo: https://github.com/spring-projects/spring-boot/blob/v1.3.0.RC1/spring-boot/src/main/java/org/springframework/boot/env/EnumerableCompositePropertySource.java release version: v1.3.0.RC1  asset relative path: spring-boot/src/main/java/org/springframework/boot/env/EnumerableCompositePropertySource.java
+*    
+*    Copyrights:
+*    
+*    Licenses:
+*     - Apache License 2.0
+*       SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 
 public class EnumerableCompositePropertySource
     extends EnumerablePropertySource<Collection<PropertySource<?>>> {
