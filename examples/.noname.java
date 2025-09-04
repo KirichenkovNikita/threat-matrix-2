84bd24550ba57679f77b842bf341ca5690d28ec0
Kirichenkov Nikita <47207005+KirichenkovNikita@users.noreply.github.com>
2025-09-05 02:00:05 +0400

    Update .noname.java

--- examples/.noname.java
+++ examples/.noname.java
@@ -1,61 +1,52 @@
 /*
 *    ------ BEGIN LICENSE ATTRIBUTION ------
-*    
+*
 *    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
-*    
-*    Repositories:
-*     - repo: https://github.com/KirichenkovNikita/kirichenkov-threatrix release version: 02-04-2025  asset relative path: /.gitattributes
-*     - repo: https://github.com/spring-projects/spring-boot release version: v2.4.10  asset relative path: spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/livereload/Connection.java
-*     - repo: Owner/Name release version: 1  asset relative path: /AssetName.java
-*     - repo: https://github.com/spring-projects/spring-security release version: 02-04-2025  asset relative path: crypto/src/main/java/org/springframework/security/crypto/codec/Base64.java
-*     - repo: https://github.com/json-iterator/go release version: 02-04-2025  asset relative path: type_tests/map_key_test.go
-*    
-*    Copyrights:
-*     - copyright 2012-2021 the original author or authors
-*    
+*
+*    Source File: spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/beans/BeansEndpoint.java
 *    Licenses:
-*     - Apache License 2.0
-*       SPDXId: Apache-2.0
-*    
+*      Eclipse Public License 2.0
+*      SPDXId: EPL-2.0
+*
 *    Auto-attribution by Threatrix, Inc.
-*    
+*
 *    ------ END LICENSE ATTRIBUTION ------
 */
 private void readWebSocketFrame() throws IOException {
-		try {
-			Frame frame = Frame.read(this.inputStream);
-			if (frame.getType() == Frame.Type.PING) {
-				writeWebSocketFrame(new Frame(Frame.Type.PONG));
-			}
-			else if (frame.getType() == Frame.Type.CLOSE) {
-				throw new ConnectionClosedException();
-			}
-			else if (frame.getType() == Frame.Type.TEXT) {
-				logger.debug(LogMessage.format("Received LiveReload text frame %s", frame));
-			}
-			else {
-				throw new IOException("Unexpected Frame Type " + frame.getType());
-			}
-		}
-		catch (SocketTimeoutException ex) {
-			writeWebSocketFrame(new Frame(Frame.Type.PING));
-			Frame frame = Frame.read(this.inputStream);
-			if (frame.getType() != Frame.Type.PONG) {
-				throw new IllegalStateException("No Pong");
-			}
-		}
-	}
+        try {
+            Frame frame = Frame.read(this.inputStream);
+            if (frame.getType() == Frame.Type.PING) {
+                writeWebSocketFrame(new Frame(Frame.Type.PONG));
+            }
+            else if (frame.getType() == Frame.Type.CLOSE) {
+                throw new ConnectionClosedException();
+            }
+            else if (frame.getType() == Frame.Type.TEXT) {
+                logger.debug(LogMessage.format("Received LiveReload text frame %s", frame));
+            }
+            else {
+                throw new IOException("Unexpected Frame Type " + frame.getType());
+            }
+        }
+        catch (SocketTimeoutException ex) {
+            writeWebSocketFrame(new Frame(Frame.Type.PING));
+            Frame frame = Frame.read(this.inputStream);
+            if (frame.getType() != Frame.Type.PONG) {
+                throw new IllegalStateException("No Pong");
+            }
+        }
+    }
 
     /**
-	 * Trigger livereload for the client using this connection.
-	 * @throws IOException in case of I/O errors
-	 */
-	void triggerReload() throws IOException {
-		if (this.webSocket) {
-			logger.debug("Triggering LiveReload");
-			writeWebSocketFrame(new Frame("{\"command\":\"reload\",\"path\":\"/\"}"));
-		}
-	}
+     * Trigger livereload for the client using this connection.
+     * @throws IOException in case of I/O errors
+     */
+    void triggerReload() throws IOException {
+        if (this.webSocket) {
+            logger.debug("Triggering LiveReload");
+            writeWebSocketFrame(new Frame("{\"command\":\"reload\",\"path\":\"/\"}"));
+        }
+    }
 
     private void complementComponentLicenses(ProjectManifest projectManifest) {
         // fill all components with license data
